400a80af84037f45f9e02fc35401be2027632bf3
Laurent Tshisekedi <Laurent.N.Tshisekedi@gmail.com>
2024-09-10 07:59:43 -0500

    object practice

--- objects/script.js
+++ objects/script.js
@@ -1,19 +1,49 @@
 
-let salaries = {
-    John: 100, 
-    Ann: 160, 
-    Pete: 130,
-}
+// let salaries = {
+//     John: 100, 
+//     Ann: 160, 
+//     Pete: 130,
+// }
 
-function sumOjectValues(obj){
-    let sum = 0; 
+// function sumOjectValues(obj){
+//     let sum = 0; 
+//     for (let key in obj) {
+//         if (typeof obj[key] === 'number') {
+//             sum += obj[key]; 
+//         }
+//     }
+
+//     return sum; 
+// }
+
+// console.log(sumOjectValues(salaries)); 
+
+let menu = {
+    width: 200, 
+    height: 300, 
+    title: "My menu"
+}; 
+
+function multiplyNumeric (obj) {
     for (let key in obj) {
         if (typeof obj[key] === 'number') {
-            sum += obj[key]; 
+            obj[key] *= 2; 
         }
     }
 
-    return sum; 
+    return obj; 
 }
 
-console.log(sumOjectValues(salaries)); 
+console.log(multiplyNumeric(menu))
+
+let message = "Hello"; 
+
+let phrase = message; 
+
+// console.log(phrase)
+
+message = "No"
+
+console.log(phrase); 
+
+console.log(message)
