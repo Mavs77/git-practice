9a413e735a97347172245aff2a7a096f9aec44a4
Laurent Tshisekedi <Laurent.N.Tshisekedi@gmail.com>
2024-09-13 22:07:02 -0500

    inspiration

---
+++ inspiration/script.js
@@ -0,0 +1,11 @@
+
+let button = document.getElementById('btn')
+
+button.addEventListener('click', addQuote)
+
+let paragraph = document.getElementById('quote')
+
+function addQuote() {
+    paragraph.textContent = "Hi Laurent. I want you to know that I'm very proud of you. Fight for the life that you want. You deserve to be happy :)"
+}
+
